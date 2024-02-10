2efe1e20792fb1993c10d2db409c453741ec7224
Tomas <tomas.m.m.d.rodrigues@gmail.com>
2024-02-10 18:12:52 +0000

    Change Grid Function Implemented

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 const container = document.querySelector("#container");
 
-for(let i = 0; i < 16; i++)
-{
-    const column = document.createElement('div');
-    column.classList.add('column');
-    container.appendChild(column);
-
-    for(let j = 0; j < 16; j++)
-    {
-        const div = document.createElement('div');
-        div.classList.add('square');
-        column.appendChild(div);
-        div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        })
-    }
-}
+createGrid(16);
 
 
 const gridBtn = document.querySelector("#gridBtn");
@@ -28,14 +13,41 @@ gridBtn.addEventListener('click', () => {
     }
     else
     {
-        for(let ii = 0; ii < newGrid; ii++)
-        {
-            
-            for(let jj = 0; jj < newGrid; jj++)
-            {
+        deleteGrid();
+        createGrid(newGrid);
+    }
+})
 
-            }
+
+
+function createGrid(newGrid)
+{
+    let width = 960 / newGrid;
+    for(let ii = 0; ii < newGrid; ii++)
+    {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        container.appendChild(column);
+
+        for(let jj = 0; jj < newGrid; jj++)
+        {
+            const div = document.createElement('div');
+            div.classList.add('square');
+            column.appendChild(div);
+            div.style.width =  width +"px";
+            div.style.height = width +"px";
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+            })
         }
     }
+}
 
-})
+
+function deleteGrid()
+{
+    while(container.firstChild)
+    {
+        container.removeChild(container.firstChild);
+    }
+}
